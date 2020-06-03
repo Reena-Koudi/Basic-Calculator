bc4542877ccee5d8bef8bbee2d34cb7e7aaaa230
Reena Koudi <reena.koudi@holition.com>
2020-06-03 16:38:39 +0100

    adding the event listener to the button click

---
+++ index.js
@@ -0,0 +1,14 @@
+//get the buttons from the DOM
+const buttons = document.querySelectorAll('.btn-number, .btn-operator');
+const clearButton = document.querySelector('.btn-clear');
+const saveButton = document.querySelector('.btn-save');
+const display = document.querySelector('.display');
+
+//add an eventListener to each of the buttons
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.getAttribute('data-num');
+    console.log('button pressed', buttonValue);
+    display.textContent += buttonValue;
+  });
+});
