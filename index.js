3433fe535f2bf631989d0dc5625b7a4e66b69a97
Reena Koudi <reena.koudi@holition.com>
2020-06-03 16:59:58 +0100

    adding the equal() to evaluate the input

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const buttons = document.querySelectorAll('.btn-number, .btn-operator');
 const clearButton = document.querySelector('.btn-clear');
 const saveButton = document.querySelector('.btn-save');
 const display = document.querySelector('.display');
+const history = document.querySelector('.history');
+const input = document.querySelector('.input');
 
 //add an eventListener to each of the buttons
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const buttonValue = button.getAttribute('data-num');
     console.log('button pressed', buttonValue);
-    display.textContent += buttonValue;
+    history.textContent += buttonValue;
+    console.log(history.textContent);
   });
 });
 
 function allClear() {
-  display.textContent = '';
+  input.textContent = '';
+  history.textContent = '';
+}
+
+function equal() {
+  input.textContent = eval(history.textContent);
+  console.log(history.textContent);
+  console.log(input.textContent);
 }
