f55f6501775056a68b429fb87e184d6fa3984ead
Reena Koudi <reena.koudi@holition.com>
2020-06-04 12:00:32 +0100

    reading the data from csv and printing it to the table format

--- Calculations.php
+++ Calculations.php
@@ -1,17 +1,45 @@
+<style>
+<?php include 'style.css'; ?>
+</style>
 <?php 
-
-	$ip_address = $_SERVER['REMOTE_ADDR']; 
+  $ip_address = $_SERVER['REMOTE_ADDR']; 
 	$browser = $_SERVER['HTTP_USER_AGENT'];
 	$date = date('d-m-Y');
 	$sum = $_POST['sum'];
-	$form_data = array($sum,$ip_address, $date,$browser );
-	$file_open  =fopen('file.csv','a');
-	$no_rows = count(file("file.csv"));
+	$form_data = array($sum,$ip_address,$date,$browser);
+	$file_open  =fopen('sum.csv','a');
+	$no_rows = count(file("sum.csv"));
+	
 	if($no_rows > 1)
 	{
 	$no_rows = ($no_rows - 1) + 1;
 	}
 
 fputcsv($file_open, $form_data);
+
+if (($handle= fopen("sum.csv", "r")) !== FALSE) 
+{
+  echo '<table>';
+  echo '<tr>';
+  echo '<th>Expression</th>';
+  echo '<th>IP</th>';
+  echo '<th>Date</th>';
+  echo '<th>Browser</th>';
+  echo '</tr>';
+  
+  while (($data = fgetcsv($handle,0, ",")) !== FALSE) 
+  {	
+    echo '<tr>';
+    echo '<th>'.$data[0].'</th>';
+    echo '<th>'.$data[1].'</th>';
+    echo '<th>'.$data[2].'</th>';
+    echo '<th>'.$data[3].'</th>';
+    echo '</tr>';
+  }
+  echo '</table>';
+
+  // Close the file
+	fclose($handle);
+}
 ?>
     
